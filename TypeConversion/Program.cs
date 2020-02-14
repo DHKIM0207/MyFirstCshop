d2a0fc0e1632f2d71700a8589d96c440d3cee272
donghyunkimmm <donghyunkim.me@gmail.com>
2020-02-14 16:06:24 +0900

    C# 기초 (프로그래밍 기초) 공부

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = 'a';
+
+            int int1 = char1;
+            Console.WriteLine("int1: " + int1);
+
+            long long1 = char1;
+            Console.WriteLine("long1: " + long1);
+
+            ulong ulong1 = char1;
+            Console.WriteLine("ulong1: " + ulong1);
+
+            float float1 = char1;
+            Console.WriteLine("float1: " + float1);
+
+            double double1 = char1;
+            Console.WriteLine("double1: " + double1);
+
+            long long2 = long.MaxValue;
+            int long2ToInt = (int)long2;
+
+            Console.WriteLine("long2: " + long2);
+            Console.WriteLine("long2ToInt: " + long2ToInt);
+
+            float float2 = 3.14159f;
+            int float2Toint = (int)float2;
+
+            Console.WriteLine("float2: " + float2);
+            Console.WriteLine("float2ToInt: " + float2Toint);
+
+            int int2 = -1;
+            uint int2ToUInt = (uint)int2;
+            Console.WriteLine("int2ToUInt: " + int2ToUInt);
+
+            int intMaxValue = int.MaxValue;
+            Console.WriteLine("intMaxValue: " + intMaxValue);
+
+            int intMinValue = int.MinValue;
+            Console.WriteLine("intMinValue: " + intMinValue);
+
+            uint uintMaxValue = uint.MaxValue;
+            Console.WriteLine("uintMaxValue: " + uintMaxValue);
+
+            uint uintMinValue = uint.MinValue;
+            Console.WriteLine("uintMinValue: " + uintMinValue);
+
+        }
+    }
+}
