49fce0d5cdbcb59b41723cbf959dcc77081d69ef
donghyunkim <donghyunkim.me@gmail.com>
2020-02-15 22:57:58 +0900

    C# 키보드로부터 숫자(정수, 부동소수) 입력받기

--- GetNumbers/Program.cs
+++ GetNumbers/Program.cs
@@ -6,13 +6,19 @@ namespace GetNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first number: ");
+            Console.WriteLine("Enter the first number (Integer): ");
             string num1 = Console.ReadLine();
 
-            Console.WriteLine("Enter the second number: ");
+            Console.WriteLine("Enter the second number (Integer): ");
             string num2 = Console.ReadLine();
 
-            Console.WriteLine($"Num1: {num1}, Num2: {num2}");
+            Console.WriteLine("Enter the third number (Integer): ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the forth number (floating number): ");
+            float num4 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Num1: {num1}, Num2: {num2}, Num3: {num3}, Num4: {num4}");
         }
     }
 }
