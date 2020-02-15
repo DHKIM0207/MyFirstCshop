e4cab3b8c5ad26677d2e4c0f4ad7f3b7e13fcfc3
donghyunkim <donghyunkim.me@gmail.com>
2020-02-15 18:09:25 +0900

    C# 문자열 포맷팅 (최신버전, 더 좋음)

---
+++ PrintStudentInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PrintStudentInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lulu";
+            int id = 20190001;
+
+            // 방법1
+            String message1 = string.Format($"Student Name: {name} / Student ID: {id}");
+            Console.WriteLine(message1);
+
+            // 방법2
+            Console.WriteLine($"Student Name: {name} / Student ID: {id}");
+        }
+    }
+}
