6041b5e6aa7050be6e7e3d41e55d4a87242ed3bf
donghyunkim <donghyunkim.me@gmail.com>
2020-02-15 22:34:53 +0900

    C# 키보드로부터 두 숫자를 읽어오기

---
+++ GetNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GetNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the second number: ");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine($"Num1: {num1}, Num2: {num2}");
+        }
+    }
+}
