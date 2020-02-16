baa05a14ce6f8a8b4017d46a5d19ccb17c6cc9d1
donghyunkim <donghyunkim.me@gmail.com>
2020-02-16 16:07:00 +0900

    C# 논리연산자, 논리표현식

---
+++ LogicalOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LogicalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int num = 10;
+
+			if (3 * 6 != 18 || ++num == 11)
+			{
+				num++;
+			}
+
+			if (12 + 2 == 14 && num++ == 11)
+			{
+				num++;
+			}
+			
+			if (1 * 4 == 4 || num++ > 0)
+			{
+				num++;
+			}
+
+			Console.WriteLine(num);
+		}
+    }
+}
